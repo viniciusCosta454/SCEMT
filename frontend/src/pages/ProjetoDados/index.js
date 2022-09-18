ce2ec5f019586280793b7dd60d4fd602153a89ed
Victor Nemo <moreira.negrelli@pucpr.edu.br>
2022-09-18 20:55:09 -0300

    Migracao das funcionalidades da pagina nova sprint

--- frontend/src/pages/ProjetoDados/index.js
+++ frontend/src/pages/ProjetoDados/index.js
@@ -1,5 +1,5 @@
-import React,{ useState, useEffect } from "react";
-import  {Link,useHistory} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { FiPower } from "react-icons/fi";
 
 import "./style.css";
@@ -7,6 +7,7 @@ import "./style.css";
 import logoImg from "../../assets/logo.png"
 
 import api from "../../services/api";
+import SprintNew from "../SprintNew";
 
 export default function Projeto() {
 
@@ -17,6 +18,18 @@ export default function Projeto() {
   const empresaName = localStorage.getItem("nomeEmpresa");
   const empresaId = localStorage.getItem("empresaId");
   const projetoId = localStorage.getItem("idProjeto");
+
+
+
+  // Dados dos componentes de cada sprint, por ora 1 sprint equivale a 1 componente: Justificar no artigo o porque!
+  const nomeComponente = localStorage.getItem("nome");
+  const tipoComponente = localStorage.getItem("tipo");
+  const iPlanComponente = localStorage.getItem("iPlan");
+  const tRelPlanComponente = localStorage.getItem("tRelPlan");
+  const tamPlanComponente = localStorage.getItem("tamPlan");
+  const itensRComponente = localStorage.getItem("itensR");
+  const tamRComponente = localStorage.getItem("tamR");
+
 
   useEffect(() => {
     api
@@ -31,21 +44,21 @@ export default function Projeto() {
       });
   }, [empresaId]);
 
-  function handleLogout(){
+  function handleLogout() {
 
-      localStorage.clear()
+    localStorage.clear()
     history.push('/')
   }
 
-  
-  
+
+
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="SCEMT" />
         <span>Bem vindo, {empresaName}</span>
-        
-        
+
+
         <Link className="button" to="/sprintNew">
           Nova Sprint
         </Link>
@@ -53,45 +66,85 @@ export default function Projeto() {
           <FiPower size={18} color="#38b6ff"></FiPower>
         </button>
       </header>
-        {
+      {
+        // eslint-disable-next-line
+        Projetos.map((Projeto) => {
           // eslint-disable-next-line
-          Projetos.map((Projeto) => {
-            // eslint-disable-next-line
-            if (projetoId == Projeto.id){
-              return (
-                <div>
-              <h1>Dados do projeto {Projeto.nome}</h1>
-              <ul>
-              <li key={Projeto.id}>
-            <strong>NOME DO PROJETO :</strong>
-            <p>{Projeto.nome}</p>
-            
-            <strong>EQUIPE RESPONSÁVEL :</strong>
-            <p>
-              {Projeto.equipe_id.map((equipe) => {
-                return `${equipe.nome} - `
-              })}
-            </p>
+          if (projetoId == Projeto.id) {
+            return (
+              <div>
+                <h1>Dados do projeto {Projeto.nome}</h1>
+                <br />
+                <div className="row">
+                  <div className="col-6">
+                    <ul>
+                      <li key={Projeto.id}>
+                        <strong>NOME DO PROJETO :</strong>
+                        <p>{Projeto.nome}</p>
 
-            <strong>DATA INICIAL DO PROJETO :</strong>
-            <p>{Projeto.dataI}</p>
+                        <strong>EQUIPE RESPONSÁVEL :</strong>
+                        <p>
+                          {Projeto.equipe_id.map((equipe) => {
+                            return `${equipe.nome} - `
+                          })}
+                        </p>
 
-            <strong>DATA FINAL DO PROJETO :</strong>
-            <p>{Projeto.dataF}</p>
+                        <strong>DATA INICIAL DO PROJETO :</strong>
+                        <p>{Projeto.dataI}</p>
 
-            
-            
-          </li>
-              </ul>
+                        <strong>DATA FINAL DO PROJETO :</strong>
+                        <p>{Projeto.dataF}</p>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
-              )
-            }
-          })
-        }
-
-      
-      
-      
+            )
+          }
+        })}
     </div>
-  );
+  )
 }
+
+
+
+return (
+        for (let index = 0; index < SprintNew.length; index++) {
+  SprintNew.map((Componente) => {
+    return (
+      <div className="col-6">
+        <div className="row">
+          <div className="col-3 text-center header">
+            <p className="dadosSprint">Sprint.map(())</p>
+          </div>
+          <div className="col-2 text-center header">
+            <p className="dadosSprint">Sprint.map[id].tipo</p>
+          </div>
+          <div className="col-5">
+            <div className="row">
+              <div className="col-4 text-center header">
+                <p className="dadosSprint">Sprint.map[id].itensPlanejados</p>
+              </div>
+              <div className="col-4 text-center header">
+                <p className="dadosSprint">Sprint.map[id].tamanhoRelativoPlanejado</p>
+              </div>
+              <div className="col-4 text-center header">
+                <p className="dadosSprint">Sprint.map[id].tamanhoPlanejado</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-2 text-center header">
+            <div className="row">
+              <div className="col-6 text-center header">
+                <p className="dadosSprint">Sprint.map[id].itensReal</p>
+              </div>
+              <div className="col-6 text-center header">
+                <p className="dadosSprint">Sprint.map[id].tamanhoReal</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+    })
